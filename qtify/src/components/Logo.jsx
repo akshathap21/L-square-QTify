935cd66d0eebc9805c7c297a1bd17a846ea1a25d
akshathap21 <akshathap21@gmail.com>
2024-04-08 17:44:11 +0530

    navbar

--- qtify/src/components/Logo.jsx
+++ qtify/src/components/Logo.jsx
@@ -3,7 +3,7 @@ import styles from "./Logo.module.css"
 import LogoImage from "../assets/logo.png"
 const Logo =()=>{
     return(
-        <div><img className={styles.logoimg} src={LogoImage} alt="qtify-logo" width={67}/></div>
+        <div><img className={styles.logoimg} src={LogoImage} alt="qtify-logo" width={67} height={34}/></div>
     )
 }
 
