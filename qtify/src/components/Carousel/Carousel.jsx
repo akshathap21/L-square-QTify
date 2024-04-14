03fbea2ded8d758a5ea4f23631677ce60b96c44e
akshathap21 <akshathap21@gmail.com>
2024-04-14 20:03:25 +0530

    Albums v0.1

--- qtify/src/components/Carousel/Carousel.jsx
+++ qtify/src/components/Carousel/Carousel.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import '../Carousel/Carousel.module.css'
 import Card from '../Card/Card';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const Carousel = ({data }) => {
-
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+  };
 
   return (
    <>
-  
-   <Card data={data}/>
+    <Slider {...settings}>
+  {data.map((ele)=>(
+    <Card data={ele}/>
+  ))}
+   </Slider>
    </>
   );
 };
